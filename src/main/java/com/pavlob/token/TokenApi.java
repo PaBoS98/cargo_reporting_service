a9fa189d2864b381ee09bf00a7c41fea55150e79
p.bohomaz <p.bohomaz@andersenlab.com>
2024-04-08 14:23:16 +0300

    remove dubug communication code

--- src/main/java/com/pavlob/token/TokenApi.java
+++ src/main/java/com/pavlob/token/TokenApi.java
@@ -1,10 +1,8 @@
 package com.pavlob.token;
 
-import com.pavlob.cargoReporting.model.CargoDto;
 import com.pavlob.token.model.ClientTokenRequest;
 import com.pavlob.token.model.JwtAuthenticationResponse;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +14,4 @@ public interface TokenApi {
 
     @PostMapping("/client")
     ResponseEntity<JwtAuthenticationResponse> getClientToken(@RequestBody ClientTokenRequest clientTokenRequest);
-
-    @GetMapping("/debug")
-    List<CargoDto> debug();
 }
--- src/main/java/com/pavlob/token/api/TokenApiImpl.java
+++ src/main/java/com/pavlob/token/api/TokenApiImpl.java
@@ -1,8 +1,5 @@
 package com.pavlob.token.api;
 
-import com.pavlob.cargoReporting.model.CargoDto;
-import com.pavlob.cargoReporting.model.DeliveryStatusDto;
-import com.pavlob.cargoReporting.resource.CargoTrackingApiClient;
 import com.pavlob.config.jwt.JwtTokenProvider;
 import com.pavlob.token.model.TokenLevel;
 import com.pavlob.token.TokenApi;
@@ -12,8 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 public class TokenApiImpl implements TokenApi {
@@ -30,13 +25,5 @@ public class TokenApiImpl implements TokenApi {
         jwtAuthenticationResponse.setTokenLevel(TokenLevel.CLIENT);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(jwtAuthenticationResponse);
-    }
-
-    @Autowired
-    private CargoTrackingApiClient cargoTrackingApiClient;
-
-    @Override
-    public List<CargoDto> debug() {
-        return cargoTrackingApiClient.getAllCargoWhereStatusNot(DeliveryStatusDto.ARRIVED);
     }
 }
